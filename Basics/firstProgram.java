67f9e0e05661502cba68eb43eb8795607112b44a
ashutosh-singh-03 <ashutosh5607singh@gmail.com>
2025-01-04 14:40:59 +0530

    some changes

--- Basics/firstProgram.java
+++ Basics/firstProgram.java
@@ -1,47 +1,41 @@
 public class firstProgram {
     public static void main(String[] args) {
-        System.out.println("Difference between print and println");
+        // Difference between print and println
         System.out.print("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
 
-        System.out.println();
-        System.out.println("VARIABLES AND THEIR DECLARATION");
+        // VARIABLES AND THEIR DECLARATION
         int x; // declaration
         x = 6; // initialization
         System.out.println(x);
 
-        System.out.println();
-        System.out.println("Variable Value Updation");
+        // Variable Value Updation
         int y = 6;
         System.out.println(y);
         y = y + 90;
         System.out.println(y);
 
-        System.out.println();
-        System.out.println("ARITHMETIC OPERATIONS");
+        // ARITHMETIC OPERATIONS
         int num1 = 6, num2 = 2;
         System.out.println(num1 + num2);
         System.out.println(num1 - num2);
         System.out.println(num1 * num2);
         System.out.println(num1 / num2);
 
-        System.out.println();
+        // Area of Circle
         double radius = 5.0;
-        System.out.println("Calculating the Area of Circle of radius " + radius);
         double area = 3.14 * radius * radius;
-        System.out.println(area);
+        System.out.println("Area of the circle of radius" + radius + "is: " + area);
 
-        System.out.println();
-        System.out.println("Calculating volume of Sphere");
-        double rad = 5f;
+        // Volume of Sphere
+        double rad = 5;
         double volume = (4 * 3.14 * rad * rad * rad) / 3;
         System.out.println("The volume of the sphere of radius " + rad + " is: " + volume);
 
-        System.out.println();
-        System.out.println("Finding the Simple Interest:");
+        // Simple Interest
         double principal = 10000, rate = 5, time = 5;
         double interest = (principal * rate * time) / 100;
-        System.out.println("Interest: " + interest);
+        System.out.println("Simple Interest: " + interest);
     }
 }
