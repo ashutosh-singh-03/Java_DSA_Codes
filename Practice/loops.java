9c3cfd9a01bf46a4a3fefc5076dc9606b4b9948b
ashutosh-singh-03 <ashutosh5607singh@gmail.com>
2025-01-13 02:27:21 +0530

    Practiced Code

---
+++ Practice/loops.java
@@ -0,0 +1,69 @@
+package Practice;
+
+import java.util.*;
+
+public class loops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Program to count the number of digits in a given number
+        System.out.print("Enter any number: ");
+        int num = sc.nextInt();
+        System.out.println("Given Number: " + num);
+
+        int digit = 0;
+        int i = num;
+        while (i != 0) {
+            i /= 10;
+            digit++;
+        }
+        System.out.print("Number of digits: " + digit);
+
+        // WAP to print the sum of the digits of the number
+        int sum = 0;
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println("\nSum of the digits: " + sum);
+
+        // WAP to find the sum of the natural numbers upto given positive integer n
+        System.out.print("Enter a positive integer: ");
+        int n = sc.nextInt();
+
+        int sum1 = 0;
+        for (int j = 1; j <= n; j++) {
+            sum1 += j;
+        }
+        System.out.println("Total Sum: " + sum1);
+
+        // WAP to find the greatest common divisor of two numbers using a loop
+        
+
+        // WAP to check if the given string is palindrome or not
+        
+
+        // WAP to generate and print the first 'n' terms of the fibonacci series
+        System.out.println("Enter the endpoint: ");
+        int end = sc.nextInt();
+
+        int firstTerm = 0, secondTerm = 1;
+        
+        for (int l = 1; l <= end; l++) {
+            System.out.print(firstTerm + " ");
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
+
+        // WAP to find to take input and do the sum until a given condition
+        int k = 1;
+        int sum2 = 0;
+        while (k <= n) {
+            int num1 = sc.nextInt();
+            sum2 += num1;
+            System.out.println("Sum: " + sum2);
+            k++;
+        }
+        sc.close();
+    }
+}
---
+++ Practice/primeComposite.java
@@ -0,0 +1,29 @@
+package Practice;
+
+import java.util.*;
+
+public class primeComposite {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter any number: ");
+        int num = sc.nextInt();
+
+        if (num <= 1)
+            System.out.println("Neither Prime nor Composite.");
+        else {
+            boolean isComposite = false;
+            for (int i = 1; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isComposite = true;
+                    break;
+                }
+            }
+            if (isComposite)
+                System.out.println(num + " is Prime.");
+            else
+                System.out.println(num + " is Composite.");
+        }
+        sc.close();
+    }
+}
