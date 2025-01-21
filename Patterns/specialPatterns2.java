25e47ed44d3b201402af982f0e5fcea8bbf28b6f
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-01-22 01:16:10 +0530

    Binary Triangle

--- Patterns/specialPatterns2.java
+++ Patterns/specialPatterns2.java
@@ -31,6 +31,15 @@ public class specialPatterns2 {
             }
             System.out.println();
         }
+
+        // Printing Binary Right Traingle
+        for(int i = 1; i <= rows; i ++){
+            for(int j = 1; j <= i; j++){
+                if((i + j) % 2 == 0) System.out.print(1);
+                else System.out.print(0);
+            }
+            System.out.println();
+        }
         sc.close();
     }
 }
