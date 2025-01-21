f9bbc7e4f3a7326a5ca09394e4aa82ef29c5bacd
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-01-22 01:02:43 +0530

    Special Patterns - Star Plus

---
+++ Patterns/specialPatterns2.java
@@ -0,0 +1,23 @@
+package Patterns;
+
+import java.util.*;
+
+public class specialPatterns2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Printing Star Plus +
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+
+        int mid = (1 + rows) / 2;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows; j++) {
+                if(i == mid || j == mid) System.out.print("* ");
+                else System.out.print("  ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
