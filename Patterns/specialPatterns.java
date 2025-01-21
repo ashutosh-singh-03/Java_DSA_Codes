a85d80d749451b62675b7a669963e003dde8d5fd
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-01-22 01:33:12 +0530

    Modified Code

--- Patterns/specialPatterns.java
+++ Patterns/specialPatterns.java
@@ -31,6 +31,52 @@ public class specialPatterns {
             }
             System.out.println();
         }
+
+        // Printing Star Plus +
+        int mid = (1 + rows) / 2;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows; j++) {
+                if (i == mid || j == mid)
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+
+        // Printing Cross X
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows; j++) {
+                if (i == j || (i + j) == (1 + rows))
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+
+        // Printing Binary Right Traingle
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0)
+                    System.out.print(1 + " ");
+                else
+                    System.out.print(0 + " ");
+            }
+            System.out.println();
+        }
+
+        // Printing Flip Right Triangle
+        // Method 1
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows; j++) {
+                if (i + j > rows)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
         sc.close();
     }
 }
