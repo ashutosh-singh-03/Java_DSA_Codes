ed786d245b654db9908b70a7622725e1ffa63f3a
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-01-22 13:25:44 +0530

    Changes Made

--- Patterns/compositePatterns.java
+++ Patterns/compositePatterns.java
@@ -2,28 +2,29 @@ package Patterns;
 
 import java.util.Scanner;
 
-public class compositePatterns{
- public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-   
-    System.out.print("Enter the number of rows: ");
-    int rows = sc.nextInt();
+//Composite Patterns - combining two patterns
+public class compositePatterns {
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
 
-   //  Printing The Following
-   // # # # *      # # #      * 
-   // # # * * ===  # #     +  * * 
-   // # * * *      #          * * * 
-   // * * * *                 * * * * 
+      System.out.print("Enter the number of rows: ");
+      int rows = sc.nextInt();
 
-   for(int i = 1; i <= rows; i++){
-      for(int j = 1; j <= rows - i; j++){
-         System.out.print("  ");
+      // Printing The Following
+      // # # # * # # # *
+      // # # * * === # # + * *
+      // # * * * # * * *
+      // * * * * * * * *
+
+      for (int i = 1; i <= rows; i++) {
+         for (int j = 1; j <= rows - i; j++) { // spaces
+            System.out.print("  ");
+         }
+         for (int j = 1; j <= i; j++) { // stars
+            System.out.print("* ");
+         }
+         System.out.println();
       }
-      for(int j = 1; j <= i; j++){
-         System.out.print("* ");
-      }
-      System.out.println();
+      sc.close();
    }
-    sc.close();
- }   
 }
