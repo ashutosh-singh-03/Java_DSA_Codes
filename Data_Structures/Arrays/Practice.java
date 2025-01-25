c3dcc014c184291db169be9c5e49c47947669b91
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-01-25 23:28:21 +0530

    Fixes and removed class files

--- Data_Structures/Arrays/Practice.java
+++ Data_Structures/Arrays/Practice.java
@@ -5,41 +5,45 @@ import java.util.Scanner;
 public class Practice {
     public static int[] inputArray(int size) {
         Scanner sc = new Scanner(System.in);
-        int arr[] = new int[size];
+        int[] arr = new int[size];
+
         for (int i = 0; i < size; i++) {
             System.out.print("Enter element " + i + ": ");
             arr[i] = sc.nextInt();
         }
+
         return arr;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // 1. Given an array for the marks of students, if the marks is less than 35
-        // give its roll (roll = index + 1)
+
+        // Task 1: Identify students with marks less than 35 and print their roll
+        // numbers
         System.out.print("Enter the number of students: ");
-        int student_count = sc.nextInt();
+        int studentCount = sc.nextInt();
 
-        int students[] = new int[student_count];
+        System.out.println("Enter marks of students:");
+        int[] students = inputArray(studentCount);
 
-        System.out.println("Enter marks of students: ");
-        students = inputArray(student_count);
-
-        for (int i = 0; i < student_count; i++) {
-            if (students[i] < 35)
+        System.out.println("Students with marks less than 35:");
+        for (int i = 0; i < studentCount; i++) {
+            if (students[i] < 35) {
                 System.out.println("Roll Number: " + (i + 1));
+            }
         }
 
-        // Calculate the sum and product of all the elements of the array
+        // Task 2: Calculate the sum and product of all the elements in the array
         int sum = 0;
-        int prod = 1;
+        int product = 1;
 
-        for (int i = 0; i < students.length; i++) {
-            sum += students[i];
-            prod *= students[i];
+        for (int marks : students) {
+            sum += marks;
+            product *= marks;
         }
-        System.out.print("Sum: " + sum);
-        System.out.print("\nProduct: " + prod);
+
+        System.out.println("\nSum of marks: " + sum);
+        System.out.println("Product of marks: " + product);
 
         sc.close();
     }
--- Data_Structures/Arrays/basicsArray.java
+++ Data_Structures/Arrays/basicsArray.java
@@ -3,44 +3,46 @@ package Arrays;
 import java.util.Scanner;
 
 public class basicsArray {
-    public static void main(String[] args) {
-        int[] arr1 = new int[5]; // declaration and memory allocation
 
-        // Initialization
+    public static void main(String[] args) {
+
+        // Declaration, memory allocation, and initialization of an array
+        int[] arr1 = new int[5];
         arr1[0] = 1;
         arr1[1] = 2;
         arr1[2] = 3;
         arr1[3] = 4;
         arr1[4] = 5;
 
-        // Printing Array
-        System.out.print(arr1[0] + " ");
-        System.out.print(arr1[1] + " ");
-        System.out.print(arr1[2] + " ");
-        System.out.print(arr1[3] + " ");
-        System.out.print(arr1[4] + " ");
+        // Printing the array using a loop
+        System.out.print("Array elements: ");
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i] + " ");
+        }
         System.out.println();
 
-        // Using Loops for input and output
+        // Taking input for a new array from the user
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of the array: ");
         int size = sc.nextInt();
 
+        // Declaration and initialization of a user-defined array
         int[] arr = new int[size];
 
-        System.out.println("Enter " + size + " elements of array: ");
+        System.out.println("Enter " + size + " elements for the array:");
         for (int i = 0; i < size; i++) {
             System.out.print("Enter element " + i + ": ");
             arr[i] = sc.nextInt();
         }
 
+        // Printing the user-defined array
         System.out.println();
-        System.out.print("Entered Array is: ");
-        System.out.print("[ ");
+        System.out.print("Entered Array is: [ ");
         for (int i = 0; i < size; i++) {
             System.out.print(arr[i] + " ");
         }
-        System.out.print("]");
+        System.out.println("]");
+
         sc.close();
     }
 }
