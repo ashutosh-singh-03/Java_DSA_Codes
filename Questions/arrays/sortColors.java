5d431149c93276fa15370c69bf6561b8f0959f93
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-02-04 04:11:48 +0530

    Method2 - Dutch Flag Algorithm

--- Questions/arrays/sortColors.java
+++ Questions/arrays/sortColors.java
@@ -27,5 +27,34 @@ public class sortColors {
         for (int i : arr) {
             System.out.print(i + " ");
         }
+
+        // Method 2 - Dutch Flag Algorithm -- one pass
+        // 3 pointer approach -- low, mid, high
+        int[] arr1 = { 0, 1, 2, 0, 1, 2, 0, 1, 2, 0, 1, 2 };
+
+        int low = 0, mid = 0, high = n - 1;
+        while (mid <= high) {
+            if (arr1[mid] == 0) {
+                swap(arr1, low, mid);
+                low++;
+                mid++;
+            } else if (arr1[mid] == 1)
+                mid++;
+            else {
+                swap(arr1, mid, high);
+                high--;
+            }
+        }
+
+        System.out.println("\nMethod 2");
+        for (int i : arr1) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
