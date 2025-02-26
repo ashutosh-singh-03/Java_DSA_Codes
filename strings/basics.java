ce35c5e1f30bbf1fd8307ec618c8406a59c710f5
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-02-26 17:12:42 +0530

    Strings

---
+++ strings/basics.java
@@ -0,0 +1,32 @@
+package strings;
+
+import java.util.Scanner;
+
+public class basics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ashutosh Singh");
+        // Ashu
+        char[] ch = { 'A', 's', 'h', 'u' };
+        for (char c : ch) {
+            System.out.print(c);
+        }
+
+        // Variable to store string
+        String s = "Ashutosh Singh";
+        System.out.println("\n" + s);
+
+        // Taking Input
+        System.out.print("Enter Your Name: ");
+        String name = sc.nextLine();
+        String fullName = sc.next();
+
+        System.out.print("\nFirst Name: " + fullName);
+        System.out.print("\nFull Name: " + name);
+
+        // charAt() and length()
+        System.out.println("\nFirst Letter: " + name.charAt(0));
+        System.out.println("Length of name: " + name.length());
+    }
+}
