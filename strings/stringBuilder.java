c6eb25768cdd5609e2dc5bb331b544ef921a2460
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-02-27 02:24:41 +0530

    String Builder Basics

---
+++ strings/stringBuilder.java
@@ -0,0 +1,19 @@
+package strings;
+
+import java.util.Scanner;
+
+public class stringBuilder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+
+        StringBuilder str = new StringBuilder(sc.nextLine());
+
+        System.out.println("String: " + str);
+        System.out.println("Reverse String: " + str.reverse());
+
+        // setCharAt()
+        str.setCharAt(1, 'z');
+        System.out.println(str);
+    }
+}
