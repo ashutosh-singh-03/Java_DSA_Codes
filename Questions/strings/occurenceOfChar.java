ed47855076c00e87e45a03409993ee5efaee9954
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-02-28 17:38:49 +0530

    Occurence of characters in a String - using frequency array

---
+++ Questions/strings/occurenceOfChar.java
@@ -0,0 +1,29 @@
+public class occurenceOfChar {
+    public static void main(String[] args) {
+        // Ques. Print the character of the lowercase English alphabet String, which is
+        // occuring the most.
+        // We can solve this by useing a freaquency array
+
+        String input = "aaabbbsdhsjdhs";
+        int[] freq = new int[26]; // 0 - a, 1 - b etc
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            int idx = (int) ch - 97;
+            freq[idx]++;
+        }
+        int maxFreq = -1;
+        for (int i = 0; i < freq.length; i++) {
+            maxFreq = Math.max(maxFreq, freq[i]);
+        }
+        System.out.print("Max Frequency: " + maxFreq);
+        
+        System.out.print("\nCharacter(s) Occured Most: ");
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] == maxFreq) {
+                char ch = (char) (i + 97);
+                System.out.print(ch + " ");
+            }
+        }
+    }
+}
