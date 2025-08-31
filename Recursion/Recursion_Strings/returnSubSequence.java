3bab86e8aa1f2a944c87a03f61b0a6a0282a2a79
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-09-01 01:22:47 +0530

    Add methods to generate and print all subsequences of a string

---
+++ Recursion/Recursion_Strings/returnSubSequence.java
@@ -0,0 +1,40 @@
+package Recursion_Strings;
+
+import java.util.*;
+
+public class returnSubSequence {
+    static ArrayList<String> getSubSequence(String s) {
+        ArrayList<String> ans = new ArrayList<>();
+        if (s.length() == 0) {
+            ans.add("");
+            return ans;
+        }
+
+        ArrayList<String> smallAns = getSubSequence(s.substring(1));
+
+        for (String ss : smallAns) {
+            ans.add(ss);
+            ans.add(s.charAt(0) + ss);
+        }
+        return ans;
+    }
+
+    static void printSubSequence(String s, String currSS) {
+        if (s.length() == 0) {
+            System.out.print(currSS + " ");
+            return;
+        }
+        
+        char curr = s.charAt(0);
+        String remString = s.substring(1);
+        // if the current character chooses to come in the String
+        printSubSequence(remString, currSS + curr);
+        // if the current character does not choose to come in the String
+        printSubSequence(remString, currSS);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSubSequence("abc"));
+        printSubSequence("abc", "");
+    }
+}
