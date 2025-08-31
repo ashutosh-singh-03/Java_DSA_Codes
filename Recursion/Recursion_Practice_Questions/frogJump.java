94bb85370bcd3d446681acc9c7707d61fa4b0572
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-09-01 01:22:54 +0530

    Add frog jump problem solution and keypad combinations generator

---
+++ Recursion/Recursion_Practice_Questions/frogJump.java
@@ -0,0 +1,21 @@
+package Recursion_Practice_Questions;
+
+public class frogJump {
+    static int minimumCost(int[] cost, int idx) {
+        if (idx == cost.length - 1)
+            return 0;
+
+        int way1 = Math.abs(cost[idx] - cost[idx + 1]) + minimumCost(cost, idx + 1);
+
+        if (idx == cost.length - 2) // because at this stone there is only one option
+            return way1;
+
+        int way2 = Math.abs(cost[idx] - cost[idx + 2]) + minimumCost(cost, idx + 2);
+        return Math.min(way1, way2);
+    }
+
+    public static void main(String[] args) {
+        int[] cost = { 10, 30, 40, 20 };
+        System.out.println("Minimum Cost: " + minimumCost(cost, 0));
+    }
+}
