52d6ee2863ffe8c98ca68801b022a641f8b6798d
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-09-15 17:35:56 +0530

    Different Methods to check sorted array

--- Sorting/checkSortedArray.java
+++ Sorting/checkSortedArray.java
@@ -1,8 +1,6 @@
 public class checkSortedArray {
-    public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5};
+    static boolean sortedM1(int[] arr) {
         boolean isSorted = true;
-
         int i = 0;
         while (i + 1 < arr.length) {
             if (arr[i] > arr[i + 1]) {
@@ -11,8 +9,29 @@ public class checkSortedArray {
             }
             i++;
         }
+        return isSorted;
+    }
 
-        if (isSorted)
+    static boolean sortedM2(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] > arr[i])
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int[] arr1 = { 15, 2, 3, 4, 5 };
+
+        System.out.println("Method 1");
+        if (sortedM1(arr))
+            System.out.print("The array is sorted.");
+        else
+            System.out.print("The array is not sorted.");
+
+        System.out.println("\nMethod 2");
+        if (sortedM2(arr1))
             System.out.print("The array is sorted.");
         else
             System.out.print("The array is not sorted.");
