25759b38ec4acaaac7b326d26a95035cddedf7eb
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-09-16 02:29:48 +0530

    Added two new methods to moveZeroes - bubble sort - one pass solution

--- Questions/Sorting/moveZeroes.java
+++ Questions/Sorting/moveZeroes.java
@@ -3,6 +3,7 @@ package Questions.Sorting;
 import java.util.ArrayList;
 
 public class moveZeroes {
+
     public static void print(int[] arr) {
         for (int i : arr) {
             System.out.print(i + " ");
@@ -10,7 +11,46 @@ public class moveZeroes {
         System.out.println();
     }
 
-    public static void method1(int[] arr) {
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void moveZerosBruteForce(int[] arr) {
+        int noz = 0;
+        for (int ele : arr) {
+            if (ele == 0)
+                noz++;
+        }
+
+        for (int x = 0; x < noz; x++) {
+            for (int i = 0; i < arr.length - 1 - x; i++) {
+                if (arr[i] == 0) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Brute Force: ");
+        print(arr);
+    }
+
+    static void moveZerosBubbleSort(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] == 0) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+        System.out.println("Bubble Sort: ");
+        print(arr);
+    }
+
+    public static void moveZerosArrayList(int[] arr) {
         ArrayList<Integer> nums = new ArrayList<>();
         int noz = 0; // number of zeroes
 
@@ -27,35 +67,35 @@ public class moveZeroes {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = nums.get(i);
         }
+
+        System.out.println("Array List: ");
+        print(arr);
     }
 
-    public static void method2(int[] arr) {
-        int noz = 0;
-        for (int ele : arr) {
-            if (ele == 0)
-                noz++;
-        }
-
-        for (int x = 0; x < noz; x++) {
-            for (int i = 0; i < arr.length - 1 - x; i++) {
-                if (arr[i] == 0) {
-                    int temp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = temp;
-                }
+    public static void moveZerosNoExtraSpace(int[] arr) {
+        int n = arr.length;
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                if (i != j)
+                    swap(arr, i, j);
+                j++;
             }
         }
+
+        System.out.println("Without any extra space: ");
+        print(arr);
     }
 
     public static void main(String[] args) {
-        int[] arr = { 0, 1, 0, 3, 12 };
+        int[] arr = { 1, -2, 3, 0, 4, 5, 0, 8 };
         System.out.println("Original Array: ");
         print(arr);
 
-        // method1(arr);
-        method2(arr);
-
-        System.out.println("Result Array: ");
-        print(arr);
+        System.out.println("Sorted Arrays: \n");
+        moveZerosBruteForce(arr);
+        moveZerosBubbleSort(arr, arr.length);
+        moveZerosArrayList(arr);
+        moveZerosNoExtraSpace(arr);
     }
 }
