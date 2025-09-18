a1a8c10ca2c2f77fb3986307945c05d6b7fe54d4
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-09-18 20:51:22 +0530

    - code for kth smallest element using sorting

---
+++ src/Questions/Sorting/kthSmallestElement.java
@@ -0,0 +1,34 @@
+package Questions.Sorting;
+
+import java.util.Scanner;
+
+public class kthSmallestElement {
+    public static int kSmallestElement(int[] arr, int k) {
+        if (k > arr.length) return -1;
+        else {
+            for (int i = 0; i < k; i++) {
+                int min = Integer.MAX_VALUE, minIdx = -1;
+                for (int j = i; j < arr.length; j++) {
+                    if (arr[j] < min) {
+                        min = arr[j];
+                        minIdx = j;
+                    }
+                }
+                int temp = arr[i];
+                arr[i] = arr[minIdx];
+                arr[minIdx] = temp;
+            }
+            return arr[k - 1];
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {3, 5, 2, 6, 9};
+
+        System.out.println("Enter the value of K: ");
+        int k = sc.nextInt();
+
+        System.out.println(kSmallestElement(arr, k));
+    }
+}
