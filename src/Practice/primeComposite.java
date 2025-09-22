6c68530756c5ace27cded5ae833f0caee13ff70f
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-09-22 14:36:42 +0530

    Corrected and Modified the Prime Number Code

--- src/Practice/primeComposite.java
+++ src/Practice/primeComposite.java
@@ -2,6 +2,7 @@ package Practice;
 
 import java.util.*;
 
+//if 'i' is a factor of n then 'n/i' will also be a factor of n
 public class primeComposite {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -9,21 +10,15 @@ public class primeComposite {
         System.out.print("Enter any number: ");
         int num = sc.nextInt();
 
-        if (num <= 1)
-            System.out.println("Neither Prime nor Composite.");
-        else {
-            boolean isComposite = false;
-            for (int i = 1; i <= Math.sqrt(num); i++) {
-                if (num % i == 0) {
-                    isComposite = true;
-                    break;
-                }
+        boolean isPrime = true;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
             }
-            if (isComposite)
-                System.out.println(num + " is Prime.");
-            else
-                System.out.println(num + " is Composite.");
         }
-        sc.close();
+        if (num == 1) System.out.println(num + " is Neither Prime nor Composite.");
+        else if (isPrime) System.out.println(num + " is Prime.");
+        else System.out.println(num + " is Composite.");
     }
 }
