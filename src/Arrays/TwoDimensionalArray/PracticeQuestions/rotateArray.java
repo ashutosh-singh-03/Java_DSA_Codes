f21db1e7524c369a96eb000ce1d5801f909ecf6d
Ashutosh Singh <ashutosh5607singh@gmail.com>
2025-09-27 12:46:05 +0530

    - updated rotate array by 90, clockWise and counterClockWise

--- src/Arrays/TwoDimensionalArray/PracticeQuestions/rotateArray.java
+++ src/Arrays/TwoDimensionalArray/PracticeQuestions/rotateArray.java
@@ -17,33 +17,55 @@ public class rotateArray {
         arr[x][y] = temp;
     }
 
-    public static void main(String[] args) {
-        int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-        int m = arr.length;
+    public static int[][] transpose(int[][] arr) {
+        int n = arr.length;
+        int[][] trans = new int[n][n];
 
-        System.out.println("Original Array: ");
-        print(arr);
-
-        // Transpose
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j <= i; j++) {
-                swap(arr, i, j, j, i);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                trans[j][i] = arr[i][j]; // just flip row & column
             }
         }
-        System.out.println("Transposed Array: ");
-        print(arr);
+        return trans;
+    }
 
-        // Rotation
-        for (int i = 0; i < m; i++) {
-            int a = 0, b = m - 1;
+    public static void rotateClockWise(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int a = 0, b = arr.length - 1;
             while (a < b) {
                 swap(arr, i, a, i, b);
                 a++;
                 b--;
             }
         }
+    }
 
-        System.out.println("Rotated Array: ");
-        print(arr);
+    public static void rotateCounterClockWise(int[][] arr) {
+        for (int j = 0; j < arr.length; j++) {
+            int a = 0, b = arr.length - 1;
+            while (a < b) {
+                swap(arr, a, j, b, j);
+                a++;
+                b--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] arr1 = {{0, 1, 2}, {3, 4, 7}, {6, 7, 8}};
+        int[][] arr2 = {{0, 1, 2}, {3, 4, 7}, {6, 7, 8}};
+
+        System.out.println("Original Array 1: ");
+        print(arr1);
+        System.out.println("Rotated Array ClockWise: ");
+        rotateClockWise(arr1);
+        print(arr1);
+
+
+        System.out.println("Original Array 2: ");
+        print(arr2);
+        System.out.println("Rotated Array Counter Clockwise: ");
+        rotateCounterClockWise(arr2);
+        print(arr2);
     }
 }
